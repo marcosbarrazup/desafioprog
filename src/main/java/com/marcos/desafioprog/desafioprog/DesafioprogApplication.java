158f7b8abd59bf24b841051ae5fe8c182ad7f811
Marcos Barra <marcos.barra@zup.com.br>
2018-10-02 14:20:06 -0300

    Primeiro Commit

---
+++ src/main/java/com/marcos/desafioprog/desafioprog/DesafioprogApplication.java
@@ -0,0 +1,12 @@
+package com.marcos.desafioprog.desafioprog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioprogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioprogApplication.class, args);
+	}
+}
---
+++ src/test/java/com/marcos/desafioprog/desafioprog/DesafioprogApplicationTests.java
@@ -0,0 +1,16 @@
+package com.marcos.desafioprog.desafioprog;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DesafioprogApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
